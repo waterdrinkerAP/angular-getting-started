ec8b65e7aa8ae1c5daa78c0ed9b2f74c6e6d1abe
Atanas Petrov <anpetrov@technologica.com>
2022-10-11 16:58:33 +0300

    Add files for tutorial up to TypeScript Functions

---
+++ README.md
@@ -0,0 +1,4 @@
+# Angular Getting Started
+
+A place to store files for the course on:
+https://www.youtube.com/watch?v=3qBXWUpoPHo&ab_channel=freeCodeCamp.org
---
+++ TypescriptDemo/datatypes.ts
@@ -0,0 +1,51 @@
+let lname: string;
+lname = 'Nasko';
+let newname = lname.toUpperCase();
+console.log(newname);
+
+let age: number;
+age = 25;
+age = 25.5;
+
+let dob = '25';
+let result = parseInt(dob);
+
+let isValid: boolean = false;
+console.log(isValid);
+
+let empList: string[];
+let depList: Array<string>;
+empList = ['One', 'Two', 'Three'];
+
+let numList: Array<number>;
+numList = [1, 2, 3, 4, 5];
+let newNum = numList[0];
+let results = numList.filter((num) => num > 2);
+let num = numList.find((num) => num === 2);
+let sum = numList.reduce((acc, num) => acc + num);
+
+console.log(results, num, sum);
+
+const enum Color {
+    Red,
+    Green,
+    Blue
+}
+let c = Color.Blue;
+
+let swapNumbs: [number, number];
+
+function swapNumbers(num1: number, num2: number): [number, number] {
+    return [num2, num1];
+}
+
+swapNumbs = swapNumbers(10, 20);
+console.log(swapNumbs[0]);
+
+let department;
+
+department = 'IT';
+department = 10;
+
+
+let mat: number[][] = [[1, 2, 3], [4, 5, 6]];
