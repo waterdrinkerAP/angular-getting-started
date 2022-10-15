e2e15f558d365c336c7746bd8f6b31042862cbc2
Atanas Petrov <anpetrov@technologica.com>
2022-10-15 18:41:09 +0300

    Add files for tutorial up to Pipes

---
+++ AngularDemo/hotelinventoryapp/src/app/rooms/rooms.interface.ts
@@ -0,0 +1,15 @@
+export interface IRoom {
+    totalRooms?: number;
+    availableRooms?: number;
+    bookedRooms?: number;
+}
+
+export interface IRoomList {
+    roomNumber: number;
+    roomType: string;
+    amenities: string;
+    price: number;
+    photos: string;
+    checkinTime: Date;
+    checkoutTime: Date;
+}
